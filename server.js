537adefaeef9fef0e9ec52dffa0c3f53971d3909
1mt4y <t.madani@esi-sba.dz>
2023-05-19 15:24:06 +0100

    Removed debugging code

--- server.js
+++ server.js
@@ -17,22 +17,12 @@ const io = new Server(httpServer, {
 app.use(cors());
 
 io.on("connection", (socket) => {
-  console.log("connection established");
   socket.on("join-room", (roomId, userId) => {
-    console.log(
-      "join-room event received, roomId=",
-      roomId,
-      " userId=",
-      userId
-    );
     socket.join(roomId);
     socket.to(roomId).emit("user-connected", userId);
-    console.log("sent user-connected to room ", roomId);
 
     socket.on("disconnect", () => {
-      console.log("disconnect event");
       socket.to(roomId).emit("user-disconnected", userId);
-      console.log("sent user-disconnected to room ", roomId);
     });
   });
 });
